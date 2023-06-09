7247be698786ac28e928bbdeac1787febed00e5e
Jason Li <jleeluit@gmail.com>
2023-06-08 21:40:26 -0400

    terraform2tier

---
+++ terraform.tf
@@ -0,0 +1,10 @@
+#This will create the workspace in my terrafom cloud workspace via "Jlee7" organization with the tags Week22Terraform2023
+terraform {
+  cloud {
+    organization = "Jlee7"
+    
+    workspaces {
+      tags = ["Week22Terraform2023"]
+    }
+  }
+}
